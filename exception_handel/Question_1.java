a85361f2b5843158c8dda327e0e891756286a697
Sam-2634 <srat563@gmail.com>
2023-04-25 13:44:42 +0530

    Exception Question 1

--- exception_handel/Question_1.java
+++ exception_handel/Question_1.java
@@ -7,16 +7,26 @@ public class Question_1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		int a,b;
 		
 		try {
+			System.out.print("Enter the first Integer : ");
+			 a = sc.nextInt();
+			System.out.print("Enter the Second Integer : ");
+			 b = sc.nextInt();
 			
-			int ans = a/b;
-			System.out.println(ans);
+			if(b==0)
+			{
+				throw new ArithmeticException("Error : division by Zero");
+			}
+			System.out.println(a +" / "+ b+" = "+(a/b));
 		}
-		catch{
-			
+		catch(ArithmeticException e){
+			System.out.println(e.getMessage());
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error : Invalid Input");
 		}
 
 	}
